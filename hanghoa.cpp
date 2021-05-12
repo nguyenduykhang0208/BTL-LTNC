e3087b6f18717db923c543ffbae915867a56fde0
nguyenduykhang0208 <79954193+nguyenduykhang0208@users.noreply.github.com>
2021-05-12 22:45:02 +0700

    Add files via upload
    Thêm, sửa, xóa.

--- hanghoa.cpp
+++ hanghoa.cpp
@@ -111,57 +111,105 @@ void sapxeptheoten(struct hanghoa*HangHoa, int n){
 	}
 }
 
-void ghiFile(struct hanghoa*HangHoa, int n){
-	getchar();
-	char fName[30];
-	printf("Nhap ten file: ");
-	gets(fName);
-	FILE* fOut = fopen(fName, "a");
-	int i;
-	for( i = 0 ; i < n ; i++ ){
-		struct hanghoa *ds = (HangHoa+i);
-		fprintf(fOut,"%-20s %-10d %-20d %-7d/%-7d/%-7d %-20s %-20d\n", ds->ten, ds->soluong, ds->dongia, 
-		ds->Date->ngay, ds->Date->thang, ds->Date->nam, ds->Kh->tenkh, ds->Kh->sdt);
-	}
-	fclose(fOut);
-	
-}
-void xoathongtin(struct hanghoa*Hanghoa, int n){
+void xoathongtin(struct hanghoa*Hanghoa, int &n){
 	int ID;
 	int found=0;
 	printf("ID can xoa: ");
 	scanf("%d", &ID);
 	for(int i=0; i<n; i++){
-		if(Hanghoa->id == ID){
+		if( (Hanghoa+i)->id == ID){
 			found = 1;
-			for(int j=i; j<n; j++){
-				*(Hanghoa+j) = *(Hanghoa+j+1); 
-			}
+			*(Hanghoa+i) = *(Hanghoa+i+1); 	
 		}
 	}
+	n--;
 	if(found == 0 ){
 		printf("Don hang co ID %d khong ton tai.", ID);
 	}
 }
-void docFile(struct hanghoa* HangHoa, int* n){
-	FILE* fOut= fopen("hanghoa.txt" ,"r");
-	int i=0;
-	if(fOut) {
-		for(;;) {
-			struct hanghoa* ds;
-			ds=(struct hanghoa*)malloc(sizeof(struct hanghoa));
-			fscanf(fOut,"%20s %10d %20d %7d/%7d/%7d %20s %20d\n", ds->ten, &ds->soluong, &ds->dongia, 
-			&ds->Date->ngay, &ds->Date->thang, &ds->Date->nam, ds->Kh->tenkh, &ds->Kh->sdt);
-			*(HangHoa+i) = ds;
-		if(feof (fOut) ){
-			break;}
+
+void ghiFile(struct hanghoa* HangHoa, int n){
+	getchar();
+	char fName[30];
+	printf("Nhap ten file: ");
+	gets(fName);
+	FILE* fOut = fopen(fName, "w");
+	int i;
+	for( i = 0 ; i < n ; i++ ){
+		struct hanghoa* ds = (HangHoa+i);
+		fprintf(fOut,"%-10d %-20s %-10d %-20d %-7d%-7d%-7d %-20s %-20d\n",ds->id, ds->ten, ds->soluong, ds->dongia, 
+		ds->Date->ngay, ds->Date->thang, ds->Date->nam, ds->Kh->tenkh, ds->Kh->sdt);
+	}
+	fclose(fOut);
 	
-		}
+}
+
+void docFile(struct hanghoa*HangHoa, int* n){
+	FILE* fOut = fopen("hanghoa.txt" , "r");
+	int i=0;
+	if(fOut){
+		for(; ;){
+		struct hanghoa ds;
+		fscanf(fOut,"%10d %20s %10d %20d %7d%7d%7d %20s %20d\n",&ds.id, ds.ten, &ds.soluong, &ds.dongia, 
+		&ds.Date->ngay, &ds.Date->thang, &ds.Date->nam, ds.Kh->tenkh, &ds.Kh->sdt);
+		i++;
+		*(HangHoa+i)= ds;
+		if(feof(fOut)){
+		break;
+						}
+					}
 	}
 	fclose(fOut);
 	*n=i;
 }
-/*void menu(){	
+void capnhatthongtin(struct hanghoa*HangHoa, int n){
+	int ID;
+	int dem=0;
+	printf("Nhap ID muon sua: ");
+	scanf("%d", &ID);
+	for(int i=0;i<n;i++){
+		if( (HangHoa+i)->id == ID ) {
+			fflush(stdin);
+			printf("Ten hang hoa: ");
+			gets((HangHoa+i)->ten);
+			printf("So luong: ");
+			scanf("%d", &(HangHoa+i)->soluong);
+			printf("Don gia: ");
+			scanf("%d", &(HangHoa + i)->dongia);
+			fflush(stdin);
+			nhaptenKH( (HangHoa+i) ->Kh);
+			nhapngaymua( (HangHoa+i) -> Date);
+			break;
+		}
+	}
+	if(dem == 0 ){
+		printf("ID khong ton tai!");
+	}
+	
+}
+
+void themhoadon(struct hanghoa* HangHoa, int &n){
+	int them=0;
+	int sl;
+	printf("Nhap so hoa don can them: ");
+	scanf("%d", &them);
+	sl=them+n;
+	struct hanghoa* HoaDon;
+	HoaDon=(struct hanghoa*)malloc(sizeof(hanghoa) * n);
+	for(int i=0;i<n;i++){
+		 *(HoaDon+i) = *(HangHoa+i);
+	}
+	HangHoa = (struct hanghoa*)malloc(sizeof(hanghoa) * sl );
+	for(int i=0;i<n;i++){
+		*(HangHoa+i) = *(HoaDon+i);
+	}
+	
+	for(int i=n;i<sl;i++){
+		nhap(HangHoa+i);
+	}
+	n=sl;
+}
+int main(){
 	struct hanghoa* HangHoa;
 	int chon;
 	int n;
@@ -173,6 +221,8 @@ void docFile(struct hanghoa* HangHoa, int* n){
 		printf("\n4.Sap xep theo ten");
 		printf("\n5.Ghi thong tin ra file.");
 		printf("\n6.Xoa thong tin theo ID.");
+		printf("\n7.Them.");
+		printf("\n8.Sua.")
 		printf("\n0.Thoat.");
 		printf("\nBan chon?");
 		scanf("%d", &chon);
@@ -183,7 +233,7 @@ void docFile(struct hanghoa* HangHoa, int* n){
 			case 1:
 				printf("Nhap so hoa don:\n");
 				scanf("%d", &n);
-				HangHoa = (hanghoa*)malloc(sizeof(hanghoa) * n);
+				HangHoa = (struct hanghoa*)malloc(sizeof(hanghoa) * n);
 				nhapds(HangHoa,n);
 				break;
 			case 2:
@@ -202,61 +252,18 @@ void docFile(struct hanghoa* HangHoa, int* n){
 			case 6:
 				xoathongtin(HangHoa,n);
 				break;
+			case 7:
+				themhoadon(HangHoa,n);
+				break;	
+			case 8:
+				capnhatthongtin(HangHoa,n);
 			default:
 				printf("Sai chuc nang, vui long chon lai");
 				break;	
 		}
 	}while(chon);
-}*/
-int main(){
-	struct hanghoa* HangHoa;
-	int chon;
-	int n=0;
-/*	do{
-		printf("=============== MENU ===============");
-		printf("\n1.Nhap hoa don vao danh sach.");
-		printf("\n2.Xuat danh sach.");
-		printf("\n3.Tim kiem theo ten khach hang.");
-		printf("\n4.Sap xep theo ten");
-		printf("\n5.Ghi thong tin ra file.");
-		printf("\n6.Xoa thong tin theo ID.");
-		printf("\n0.Thoat.");
-		printf("\nBan chon?");
-		scanf("%d", &chon);
-		
-		switch(chon){
-			case 0:
-				break;
-			case 1:
-				printf("Nhap so hoa don:\n");
-				scanf("%d", &n);
-				HangHoa = (hanghoa*)malloc(sizeof(hanghoa) * n);
-				nhapds(HangHoa,n);
-				break;
-			case 2:
-				xuatds(HangHoa,n);
-				break;
-			case 3:
-				fflush(stdin);
-				timkiemtheoten(HangHoa,n);
-				break;
-			case 4:
-				sapxeptheoten(HangHoa,n);
-				break;
-			case 5:
-				ghiFile(HangHoa,n);
-				break;
-			case 6:
-				xoathongtin(HangHoa,n);
-				break;
-			default:
-				printf("Sai chuc nang, vui long chon lai");
-				break;	
-		}
-	}while(chon);*/
-	docFile(HangHoa,&n);
-	xuatds(HangHoa,n);
-	int i;
+
+	
 }
 
 
